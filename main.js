13a9ca8c0355b5662b7611129d42f4c9415a5cb2
Joshua Mun <joshmun23@gmail.com>
2015-09-14 15:43:28 -0400

    refactor encapsulation of reactive functions in template helpers to when a template is created

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 if (Meteor.isClient) {
   Session.setDefault('mySessionExample', 'Sup boo');
 
+  Template.home.created = function() {
+    this.autorun(function(){
+      alert(Session.get('mySessionExample'))
+    })
+  }
+
   Template.home.helpers({
-    testFunc: function(){
-      Tracker.autorun(function(c) {
-        var example = Session.get('mySessionExample');
-        if(!c.firstRun) {
-          alert(example);
-        }
-      });
-    }
+    // testFunc: function(){
+    //   Tracker.autorun(function(c) {
+    //     var example = Session.get('mySessionExample');
+    //     if(!c.firstRun) {
+    //       // this conditional stops reactivity if the session value is the same
+    //       alert(example);
+    //     }
+    //   });
+    // }
   })
   // Tracker.autorun(function() {
   //   var example = Session.get('mySessionExample');
